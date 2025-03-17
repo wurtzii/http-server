489a6474c4bdcaa8ba704c9d39683128fc772c68
gabriel <wurtzii05@gmail.com>
2025-03-17 13:52:40 -0500

    l5

--- main.go
+++ main.go
@@ -1,25 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "errors"
-    "io"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-    r, err := os.Open("messages.txt")
+    f, err := os.Open("messages.txt")
+    ch := getLinesChannel(f)
     if err != nil {
         log.Fatal(err)
     }
-    b := make([]byte, 8)
-    for {
-        _, err := r.Read(b)
-        if errors.Is(err, io.EOF) {
-            log.Fatal(err)
-        }
 
-        fmt.Printf("read: %s", string(b))
+    for s := range ch {
+        fmt.Printf("read: %s\n", s)
     }
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+    ch := make(chan string)
+    go func() {
+        defer f.Close()
+        defer close(ch)
+        var curr_line string
+        for {
+            b := make([]byte, 8, 8)
+            _, err := f.Read(b)
+            if err != nil {
+                if errors.Is(err, io.EOF) {
+                    break
+                }
+
+                fmt.Println(err)
+                break
+            }
+
+            parts := strings.Split(string(b), "\n")
+            if len(parts) > 1 {
+                for _, s := range parts[:len(parts) -1] {
+                    ch <- fmt.Sprintf("%s%s", curr_line, s)
+                    curr_line = ""
+                }
+
+                curr_line += parts[len(parts) -1]
+            } else {
+                curr_line += parts[0]
+            }
+        }
+    }()
+
+    return ch
 }
